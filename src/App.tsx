9e384c1ff207c711e89dbb78a5cab4f19f1dd99a
Leplan Ludwig <leplan.ludwig@gmail.com>
2020-07-09 12:03:31 +0200

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -11,7 +11,7 @@ import {
 } from '@ionic/react';
 
 import { IonReactRouter } from '@ionic/react-router';
-import { ellipse, square, triangle } from 'ionicons/icons';
+import { ellipse, square, triangle, locateOutline, listOutline, addOutline } from 'ionicons/icons';
 import Tab1 from './pages/Tab1';
 import Tab2 from './pages/Tab2';
 /* Core CSS required for Ionic components to work properly */
@@ -51,15 +51,15 @@ const App: React.FC = () => (
         </IonRouterOutlet>
         <IonTabBar slot="bottom">
           <IonTabButton tab="tab1" href="/tab1">
-            <IonIcon icon={triangle} />
+            <IonIcon icon={locateOutline} />
             <IonLabel>Lieu actuel</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab2" href="/tab2">
-            <IonIcon icon={ellipse} />
+            <IonIcon icon={listOutline} />
             <IonLabel>Liste météo</IonLabel>
           </IonTabButton>
           <IonTabButton tab="tab3" href="/tab3">
-            <IonIcon icon={square} />
+            <IonIcon icon={addOutline} />
             <IonLabel>Favoris</IonLabel>
           </IonTabButton>
         </IonTabBar>
--- src/pages/Tab2.tsx
+++ src/pages/Tab2.tsx
@@ -1,9 +1,24 @@
-import React, {useState, useEffect} from 'react';
-import { IonContent, IonHeader, IonPage, IonTitle, IonToolbar } from '@ionic/react';
-import ExploreContainer from '../components/ExploreContainer';
-import firebase from 'firebase'
-import helpers from '../helpers/helpers'
-import './Tab2.css';
+import React, { useState, useEffect, Fragment } from "react";
+import {
+  IonContent,
+  IonHeader,
+  IonPage,
+  IonTitle,
+  IonToolbar,
+  IonCard, 
+  IonAvatar, 
+  IonCardTitle, 
+  IonList, 
+  IonLabel,
+  IonItem,
+  IonCardContent,
+  IonCardHeader
+} from "@ionic/react";
+import ExploreContainer from "../components/ExploreContainer";
+import firebase from "firebase";
+import helpers from "../helpers/helpers";
+import moment from 'moment'
+import "./Tab2.css";
 
 
 const firebaseConfig = {
@@ -13,7 +28,7 @@ const firebaseConfig = {
   projectId: "testcesimeteo",
   storageBucket: "testcesimeteo.appspot.com",
   messagingSenderId: "79665149858",
-  appId: "1:79665149858:web:ee16bd194edbd21bcc64fb"
+  appId: "1:79665149858:web:ee16bd194edbd21bcc64fb",
 };
 
 firebase.initializeApp(firebaseConfig);
@@ -25,22 +40,28 @@ const Tab2: React.FC = () => {
 
   useEffect(() => {
     async function getWeatherFromList() {
-      const documents = await db.collection("WeatherList").get()
-        let citys = []
-        documents.forEach(doc => {
-            citys.push(doc.data())
-        });
+      const documents = await db.collection("WeatherList").get();
+      let citys = [];
+      documents.forEach((doc) => {
+        citys.push(doc.data());
+      });
 
-        const weathersList = await Promise.all(citys.map(async item => {
+      const weathersList = await Promise.all(
+        citys.map(async (item) => {
           return {
             ...item,
-            weather: await helpers.getWeather(item.city)
-          }
-        }))
-        return weathersList
-      }
-      getWeatherFromList()
-    })
+            weather: await helpers.getWeather(item.city),
+          };
+        })
+      );
+      console.log(weathersList, 'list')
+      setCitys(weathersList)
+    }
+    
+ getWeatherFromList();
+  }, []);
+
+  const formatTemperature = (temp: number) => `${(temp - 273.15).toFixed(0)}°`;
 
   return (
     <IonPage>
@@ -52,10 +73,35 @@ const Tab2: React.FC = () => {
       <IonContent>
         <IonHeader collapse="condense">
           <IonToolbar>
-            <IonTitle size="large">Tab 2</IonTitle>
+            <IonTitle size="large">Liste météo</IonTitle>
           </IonToolbar>
         </IonHeader>
-        <ExploreContainer name="Tab 2 page" />
+        {citys !== null && <Fragment>
+          {citys.map((item) => (
+        <IonCard>
+            <IonCardContent>
+              <IonList>
+                  <IonItem>
+                    <IonAvatar slot="start">
+                      <img
+                        src={`http://openweathermap.org/img/wn/${item.weather.weather[0].icon}.png`}
+                      />
+                    </IonAvatar>
+
+                    <IonLabel>
+                      <h1>{item.city}</h1>
+                      <h2>{formatTemperature(item.weather.main.temp)}</h2>
+                      <h3>{item.weather.weather[0].main}</h3>
+                      <p>{item.weather.weather[0].description}</p>
+                    </IonLabel>
+                  </IonItem>
+              </IonList>
+            </IonCardContent>
+          </IonCard>
+          ))}
+          
+          </Fragment>}
+      
       </IonContent>
     </IonPage>
   );
